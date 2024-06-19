aa509a059e68efa3a3f508b90e63c6a276d4973b
tumoyakov <leonid.tumoyakov@yandex.ru>
2024-06-19 19:05:32 +0700

    fix(Contacts): [imperial] fix layout styles

--- imperial/src/widgets/contacts/Contacts.tsx
+++ imperial/src/widgets/contacts/Contacts.tsx
@@ -1,13 +1,14 @@
 import { memo } from "react";
-import "./Contacts.css";
-import { SectionContainer } from "@entities/SectionContainer";
 import { SectionHeader } from "@entities/SectionHeader";
 import { description, title } from "./Contacts.const";
 
+import "./Contacts.css";
+import "@entities/SectionContainer/SectionContainer.css";
+
 const Contacts = memo(() => {
   return (
-    <SectionContainer id="contacts" className="contacts">
-      <section className="contacts__content">
+    <section id="contacts" className="contacts">
+      <div className="contacts__container">
         <header>
           <SectionHeader title={title} description={description} />
         </header>
@@ -20,12 +21,15 @@ const Contacts = memo(() => {
             ваши вопросы
           </p>
         </form>
-      </section>
-      <section className="contacts__map">
+      </div>
+      <div
+        className="contacts__map"
+        aria-label="Карта местонахождения офиса компании"
+      >
         {/* TODO add real map widget */}
         <img src="/src/assets/contacts/map.png" alt="map placeholder" />
-      </section>
-    </SectionContainer>
+      </div>
+    </section>
   );
 });
 
